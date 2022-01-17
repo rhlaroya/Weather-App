2515275d3b5c3cdf9ea49147122ee81736df4827
Regae Laroya <regae.laroya@collabera.com>
2022-01-17 13:19:23 -0500

    Added feels like and humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback('Unable to get forecast try another search', undefined);
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out.")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. Feels like " + body.current.feelslike + " Humidity is at " + body.current.humidity + ".")
         }
     })
 }
